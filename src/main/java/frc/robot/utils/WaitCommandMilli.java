4a66e3a19a382cce338f7f0dc69b3683cf4f6436
Milwaukee Cyber Cheese <98500141+MKECyberCheese@users.noreply.github.com>
2024-01-24 17:09:23 -0600

    add file I forgot to save

--- src/main/java/frc/robot/utils/WaitCommandMilli.java
+++ src/main/java/frc/robot/utils/WaitCommandMilli.java
@@ -2,7 +2,7 @@ package frc.robot.utils;
 
 public class WaitCommandMilli extends edu.wpi.first.wpilibj2.command.WaitCommand{
 
-    //replace generic wait command with command in milliseconds
+    //replace generic wait command with command in millisecondsw
     public WaitCommandMilli(double milliseconds) {
         super(milliseconds / 1000);
     }
