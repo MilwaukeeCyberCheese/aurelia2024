b3f54d6bfe2957757eda7b75db6e113c6d828061
Milwaukee Cyber Cheese <98500141+MKECyberCheese@users.noreply.github.com>
2024-02-10 11:08:32 -0600

    whoops, forgot that one

---
+++ src/main/java/frc/robot/commands/ShooterCommands/SetWristAngleCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.ShooterCommands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class SetWristAngleCommand extends Command {
+    public final ShooterSubsystem m_shooterSubsystem;
+    public final DoubleSupplier m_angle;
+
+    /**
+     * Set angle of the shooter
+     * 
+     * @param angle            angle to set the shooter to
+     * @param shooterSubsystem subsystem for controlling the shooter
+     *                         {@link frc.robot.subsystems.ShooterSubsystem link}
+     */
+    public SetWristAngleCommand(DoubleSupplier angle, ShooterSubsystem shooterSubsystem) {
+        m_angle = angle;
+        m_shooterSubsystem = shooterSubsystem;
+        addRequirements(m_shooterSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        m_shooterSubsystem.setPosition(m_angle.getAsDouble());
+    }
+
+    @Override
+    public boolean isFinished() {
+        return m_shooterSubsystem.atPosition();
+    }
+}
