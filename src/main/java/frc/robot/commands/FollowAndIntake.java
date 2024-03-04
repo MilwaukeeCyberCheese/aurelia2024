7611050d2deeedfe4b940d362cef477c6b8f6258
Sam948-byte <techguy763@gmail.com>
2024-03-04 15:36:59 -0600

    some code for vision tracking

--- src/main/java/frc/robot/commands/FollowAndIntake.java
+++ src/main/java/frc/robot/commands/FollowAndIntake.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.IntakeCommands.IntakeCommand;
-import frc.robot.commands.IntakeCommands.IntakeFromGround;
 import frc.robot.commands.IntakeCommands.IntakePositionCommand;
 import frc.robot.subsystems.IntakeCameraSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
