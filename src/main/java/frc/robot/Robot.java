a42abd6627e9de4e722b0d82bb50cd2e25adf6c0
Sam948-byte <techguy763@gmail.com>
2024-03-11 10:56:45 -0500

    update auto starting poses and libraries

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.GyroReset;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -81,6 +82,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
+    new GyroReset();
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // reset the pose of the robot to the starting pose of the autonomous command
