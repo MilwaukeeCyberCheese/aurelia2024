2abafda2c16145d057ca19369b4341a56afe348a
Milwaukee Cyber Cheese <98500141+MKECyberCheese@users.noreply.github.com>
2024-03-11 19:31:25 -0500

    Pulsing

---
+++ src/main/java/frc/robot/commands/IntakeCommands/Pulse.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.IntakeCommands;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.utils.WaitCommandMilli;
+
+public class Pulse extends SequentialCommandGroup {
+    private double speed = 0.3;
+    private int waitTime = 100;
+
+    public Pulse(IntakeSubsystem m_IntakeSubsystem) {
+        addCommands(Commands.race(new IntakeSpeedCommand(() -> speed, m_IntakeSubsystem),
+                new WaitCommandMilli(waitTime)), 
+                Commands.race(new IntakeSpeedCommand(() -> -speed, m_IntakeSubsystem),
+                new WaitCommandMilli(waitTime)), Commands.race(new IntakeSpeedCommand(() -> speed, m_IntakeSubsystem),
+                new WaitCommandMilli(waitTime)), 
+                Commands.race(new IntakeSpeedCommand(() -> -speed, m_IntakeSubsystem),
+                new WaitCommandMilli(waitTime)), Commands.race(new IntakeSpeedCommand(() -> speed, m_IntakeSubsystem),
+                new WaitCommandMilli(waitTime)), 
+                Commands.race(new IntakeSpeedCommand(() -> -speed, m_IntakeSubsystem),
+                new WaitCommandMilli(waitTime)), Commands.race(new IntakeSpeedCommand(() -> speed, m_IntakeSubsystem),
+                new WaitCommandMilli(waitTime))
+                );
+    }
+}
