5a2d4136af33399b1cb1fad0ae253dd3f0034b52
Sam948-byte <techguy763@gmail.com>
2024-03-13 14:26:09 -0500

    forgot to add

---
+++ src/main/java/frc/robot/commands/ScoreAmpIntake.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.IntakeCommands.SetIntakePosition;
+import frc.robot.commands.IntakeCommands.SetIntakeSpeed;
+import frc.robot.commands.ShooterCommands.SetWristAngle;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ScoreAmpIntake extends SequentialCommandGroup {
+    public ScoreAmpIntake(ShooterSubsystem shooterSubsystem, IntakeSubsystem intakeSubsystem) {
+        addCommands(
+                new SetWristAngle(() -> Constants.ShooterConstants.kIntakeSafeAngle, shooterSubsystem),
+                new SetIntakePosition(() -> Constants.IntakeConstants.kIntakeAmpPosition, intakeSubsystem),
+                new SetIntakeSpeed(() -> Constants.IntakeConstants.kAmpSpeed, intakeSubsystem));
+    }
+
+}
