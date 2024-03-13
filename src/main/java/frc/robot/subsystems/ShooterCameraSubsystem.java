4cd9e5afd25e2438723073bfd6a0eea253864c65
Sam948-byte <techguy763@gmail.com>
2024-03-13 11:16:00 -0500

    add snapToAlign

--- src/main/java/frc/robot/subsystems/ShooterCameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterCameraSubsystem.java
@@ -1,10 +1,13 @@
 //TODO make this prettier
 package frc.robot.subsystems;
 
+import java.util.List;
+
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Robot;
 
 public class ShooterCameraSubsystem extends SubsystemBase {
     private PhotonTrackedTarget target;
@@ -76,6 +79,30 @@ public class ShooterCameraSubsystem extends SubsystemBase {
                     estimate.timestampSeconds);
 
         }
+    }
+
+    public PhotonTrackedTarget[] getSpeakerTargets() {
+        PhotonTrackedTarget[] returnTargets = new PhotonTrackedTarget[2];
+        var result = Constants.VisionConstants.ShooterCamera.kCamera.getLatestResult();
+        List<PhotonTrackedTarget> targets = result.getTargets();
+        for (PhotonTrackedTarget target : targets) {
+            if (Robot.allianceColor){
+                if (target.getFiducialId() == 4){
+                    returnTargets[0] = target;
+                } else if (target.getFiducialId() == 3){
+                    returnTargets[1] = target;
+                }
+            } else{
+                if (target.getFiducialId() == 8){
+                    returnTargets[0] = target;
+                } else if (target.getFiducialId() == 7){
+                    returnTargets[1] = target;
+                }
+            }
+        }
+
+        return returnTargets;
+        
     }
 
 }
